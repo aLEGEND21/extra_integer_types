715f31673761b563cb2705edaa8fcecf12e5d4d7
aLEGEND21 <arnavmurthi@gmail.com>
2021-06-10 10:35:22 -0400

    Fixed import bugs

--- extra_integer_types/__init__.py
+++ extra_integer_types/__init__.py
@@ -1,8 +1,6 @@
-from .extra_integer_types import Prime
+from .extra_integer_types import *
 
 __all__ = ["name", "__version__", "prime"]
 
 name = "extra_integer_types"
 __version__ = "0.1.0"
-
-prime = Prime()
